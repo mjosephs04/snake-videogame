d9214a4cdbd6cca238282e1aca961b08fa1a5bf3
mjosephs04 <95541571+mjosephs04@users.noreply.github.com>
2022-12-01 16:05:34 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ const int NUM_COL = 1000;
 enum Direction {UP, DOWN, LEFT, RIGHT};
 
 int main(int argc, const char * argv[]) {
-    
+
     char key;
-    int length = 10;
+    int length = 3;
     SDL_Plotter g(NUM_ROW, NUM_COL);
     int R = 112, G = 112, B = 112;
     //int xLoc = NUM_COL / 2, yLoc = NUM_ROW / 2;
-    int xLoc[length], yLoc[length];
+    int xLoc[50], yLoc[50];
     int prevX, prevY;
     Direction dir = RIGHT;
     int speed = 100;
@@ -29,35 +29,37 @@ int main(int argc, const char * argv[]) {
     yLoc[0] = NUM_ROW / 2;
     yLoc[1] = NUM_ROW / 2;
     Fruit apple;
-    
-    srand(time(0));
-    
+    bool gameOver;
+
+    //srand(time(0));
+
     apple.setPoint((((rand()% (1000 / SIZE)) +1) * SIZE) , (((rand()% (1000 / SIZE)) +1) * SIZE));
     while(!g.getQuit()){
-    
-    
+
+
        if(g.kbhit()){
             key = g.getKey();
            switch(key){
                case UP_ARROW     : dir = UP;
                                     break;
-                   
+
                case DOWN_ARROW   : dir = DOWN;
                                     break;
-                   
+
                case LEFT_ARROW   : dir = LEFT;
                                     break;
-                   
+
                case RIGHT_ARROW  : dir = RIGHT;
                                     break;
-                   
+
            }
         }
-    
+
     //process
-        
+
     //draw black background
-        for(int y = 0; y < NUM_ROW; y++){
+    if(!gameOver){
+                for(int y = 0; y < NUM_ROW; y++){
             for(int x = 0; x < NUM_COL; x++){
                 if(y%SIZE == 0 || x%SIZE == 0){
                     g.plotPixel(x, y,20,250,20);
@@ -67,7 +69,9 @@ int main(int argc, const char * argv[]) {
                 }
             }
         }
-    
+    }
+
+
     //copy cell locations
     for(int i = length - 1; i > 0; i--){
         xLoc[i] = xLoc[i-1];
@@ -76,22 +80,23 @@ int main(int argc, const char * argv[]) {
     switch(dir){
         case RIGHT : xLoc[0] += SIZE;
                     break;
-            
+
         case LEFT  : xLoc[0] -= SIZE;
                     break;
-            
+
         case UP    : yLoc[0] -= SIZE;
                     break;
-            
+
         case DOWN  : yLoc[0] += SIZE;
                     break;
     }
     //draw background
-        
-    
-    
+
+
+
     //Draw
-    for(int i =0; i < length; i++){
+    if(!gameOver){
+        for(int i =0; i < length; i++){
         for(int y = 0; y < SIZE; y++){
             for(int x = 0; x < SIZE; x++){
                 if(i == 0){
@@ -103,17 +108,37 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-       
+
         if(xLoc[0] == apple.getPoint().x && (yLoc[0] == apple.getPoint().y)){
             apple.setPoint((((rand()% (1000 / SIZE -1 )) +1) * SIZE) , (((rand()% (1000 / SIZE -1 )) +1) * SIZE));
             length++;
         }
-        
+
         apple.drawFriut(apple.getPoint() , g);
+    }
+
+
+
+    if(xLoc[0] == -50 || xLoc[0] == NUM_COL){
+        for(int x = 0; x < NUM_COL; x++){
+            for(int y = 0; y < NUM_ROW; y++){
+                g.plotPixel(x, y, 0, 0, 0);
+            }
+        }
+        gameOver = true;
+    }
+    if(yLoc[0] == -50 || yLoc[0] == NUM_ROW){
+        for(int x = 0; x < NUM_COL; x++){
+            for(int y = 0; y < NUM_ROW; y++){
+                g.plotPixel(x, y, 0, 0, 0);
+            }
+        }
+        gameOver = true;
+    }
+
     g.update();
     g.Sleep(speed);      //takes miliseconds for its argument
-    
+
     }
     return 0;
 }
-
