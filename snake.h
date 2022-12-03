194dc6c404b6e28bafb85757f67e7c3f57827edc
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-03 14:36:15 -0600

    Add files via upload

--- snake.h
+++ snake.h
@@ -3,6 +3,7 @@
 
 #include "SDl_PLotter.h"
 #include "point.h"
+#include "constants.h"
 
 class snake{
 private:
@@ -13,8 +14,9 @@ private:
     Point_t loc[MAX];
 
 public:
-    snake(int l){
+    snake(int l, color c = color(112, 112, 112)){
         length = l;
+        this->c = c;
 
         for(int i = 0; i < MAX; i++){
             loc[i] = Point_t(-2, -2);
@@ -24,6 +26,16 @@ public:
             loc[i] = Point_t(NUM_ROW / 2 - i*SIZE,
                              NUM_COL / 2 - i*SIZE)
         }
+    }
+
+    void setColor(color c = color(112, 112, 112)){
+        this->c = c;
+    }
+    void setDir(Direction d){
+        dir = d;
+    }
+    void setLength(int l){
+        length = l;
     }
 
     void advance(){
