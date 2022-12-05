ef3cffa1f1030a8547e3bd8673eabf280f7d053c
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-05 16:18:00 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,187 +1,135 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
 #include <SDL2/SDL.h>
 #include "SDL_Plotter.h"
 #include "gameObject.h"
+#include "snake.h"
+#include "fruit.h"
 #include "sound.h"
-#include <ctime>
-
 using namespace std;
 
-
-enum Direction {UP, DOWN, LEFT, RIGHT};
-
-int main(int argc, char* argv[]) {
-
+int main(int argc, char** argv) {
+// Data Abstraction
     char key;
-    int length = 3;
-    SDL_Plotter g(NUM_ROW + 50, NUM_COL);
-    int xLoc[NUM_COL], yLoc[NUM_ROW];
-    Direction dir = RIGHT;
-    int speed = 70;
-    Game fun;
-    int score = 0;
-
-
-    for(int i = 0; i < NUM_COL; i++){
-        xLoc[i] = -2;
-    }
-    for(int i = 0; i < NUM_ROW; i++){
-        yLoc[i] = -2;
-    }
-
-    xLoc[0] = NUM_COL / 2;
-    xLoc[1] = NUM_COL / 2 - SIZE;
-    yLoc[0] = NUM_ROW / 2;
-    yLoc[1] = NUM_ROW / 2;
+    bool isPaused;
+    SDL_Plotter g(NUM_ROW, NUM_COL);
+    snake hiss(3);
     Fruit apple;
+    Direction dir;
+    Game fun;
     Sound soundEffects(g);
 
+    color black;
+
     srand(time(0));
+    apple.setPoint((((rand()% (1000 / SIZE -1 )) +1) * SIZE) ,
+                   (((rand()% (1000 / SIZE -1 )) +1) * SIZE));
 
-    apple.setPoint((((rand()%(NUM_COL / SIZE) )) * SIZE) , (rand()%(NUM_ROW / SIZE) * SIZE));
+// Main Loop
+    hiss.setColorHead();
     while(!g.getQuit()){
-       if(g.kbhit()){
+    //UI
+        if(g.kbhit()){
             key = g.getKey();
-           switch(key){
-               case UP_ARROW     : if(dir != 1){dir = UP;};
-                                    break;
 
-               case DOWN_ARROW   : if(dir != 0){dir = DOWN;};
-                                    break;
-
-               case LEFT_ARROW   : if(dir != 3){dir = LEFT;};
-                                    break;
-
-               case RIGHT_ARROW  : if(dir != 2){dir = RIGHT;};
-                                    break;
-
-               case SPACE        : if(fun.checkState() == PLAY){fun.changeState(PAUSE);}
-                                   else{fun.changeState(PLAY);}
-                                    break;
-
-               case P            : fun.changeState(PLAY);
-                                    break;
-
-           }
+            switch(key){
+                case UP_ARROW:
+                    if(hiss.getDir() != DOWN){
+                        hiss.setDir(UP);
+                    }
+                    break;
+                case DOWN_ARROW:
+                    if(hiss.getDir() != UP){
+                        hiss.setDir(DOWN);
+                    }
+                    break;
+                case LEFT_ARROW:
+                    if(hiss.getDir() != RIGHT){
+                        hiss.setDir(LEFT);
+                    }
+                    break;
+                case RIGHT_ARROW:
+                    if(hiss.getDir() != LEFT){
+                        hiss.setDir(RIGHT);
+                    }
+                    break;
+                case SPACE:
+                    if(fun.checkState() == PLAY){
+                        fun.changeState(PAUSE);
+                    }
+                    else if(fun.checkState() == PAUSE){
+                        fun.changeState(PLAY);
+                    }
+                    break;
+                case P:
+                    fun.changeState(PLAY);
+                    break;
+            }
         }
 
-    //process
-
-    if(fun.checkState() == PLAY){
-        cout << "Started" << endl;
-
-        if(fun.checkState() == PLAY){
-                for(int y = 0; y < NUM_ROW; y++){
-                    for(int x = 0; x < NUM_COL; x++){
-                        if(y%SIZE == 0 || x%SIZE == 0){
-                            g.plotPixel(x, y,20,250,20);
-                        }
-                        else{
-                            g.plotPixel(x, y,0,0,0);
-                        }
-                    }
-                }
-
-            //copy cell locations
-                for(int i = length - 1; i > 0; i--){
-                    xLoc[i] = xLoc[i-1];
-                    yLoc[i] = yLoc[i-1];
-                }
-            switch(dir){
-                case RIGHT : xLoc[0] += SIZE;
-                    break;
-
-                case LEFT  : xLoc[0] -= SIZE;
-                    break;
-
-                case UP    : yLoc[0] -= SIZE;
-                    break;
-
-                case DOWN  : yLoc[0] += SIZE;
-                    break;
-            }
-
-            //game check
-            if(xLoc[0] == -SIZE || xLoc[0] == NUM_COL){
-                fun.changeState(OVER);
-                soundEffects.dying(g);
-            }
-            if(yLoc[0] == -SIZE || yLoc[0] == NUM_ROW){
-                fun.changeState(OVER);
-                soundEffects.dying(g);
-            }
-
-            //hit detection of itself
-            for(int i = 1; i < length; i++){
-                if(xLoc[0] == xLoc[i] && yLoc[0] == yLoc[i]){
-                    fun.changeState(OVER);
-                    soundEffects.dying(g);
+        if(fun.checkState() == START){
+            for(int y = 0; y < NUM_ROW + SIZE; y++){
+                for(int x = 0; x > NUM_COL; x++){
+                    g.plotPixel(x, y, 255, 255, 255);
                 }
             }
-
-            //Draw
-            for(int i =0; i < length; i++){
-                for(int y = 0; y < SIZE; y++){
-                    for(int x = 0; x < SIZE; x++){
-                        if(i == 0){
-                            g.plotPixel(xLoc[i] + x, yLoc[i] + y, 255, 153, 0);
-
-                        }
-                        else{
-                            g.plotPixel(xLoc[i] + x, yLoc[i] + y, 112, 112, 112);
-                        }
-                    }
-                }
-
+        }
+        else if(fun.checkState() == PLAY || fun.checkState() == PAUSE){
+        // Check states
+            if(fun.checkState() == PLAY){
+                // Advance
+                hiss.advance();
             }
 
-            //check if it hits apple
-            if(xLoc[0] == apple.getPoint().x && (yLoc[0] == apple.getPoint().y)){
-                apple.setPoint((((rand()% (NUM_COL / SIZE -1 )) +1) * SIZE) , (((rand()% (NUM_ROW / SIZE -1 )) +1) * SIZE));
+            // die check
+            fun.changeState(hiss.checkDie(soundEffects, g));
+
+        // Fruit
+            if(hiss.getFirstPt().x == apple.getPoint().x &&
+               hiss.getFirstPt().y == apple.getPoint().y){
+                //apple.setPoint((((rand()% (1000 / SIZE -1 )) +1) * SIZE) ,
+                //               (((rand()% (1000 / SIZE -1 )) +1) * SIZE));
                 apple.eatenFruit();
-                length++;
+                hiss.incLength(1);
                 fun.addPoint();
-                cout << "score: " << fun.getScore() << endl;
                 soundEffects.eating(g);
-
             }
-            //draws new fruit
-            apple.drawFriut(apple.getPoint(), g);
 
-        }
-
-        else{
-            //if is paused, show black screen
+        // Draw
+            // Draw background
             for(int y = 0; y < NUM_ROW; y++){
                 for(int x = 0; x < NUM_COL; x++){
-                    g.plotPixel(x, y,0,0,0);
+                    if(y%SIZE == 0 || x%SIZE == 0){
+                        g.plotPixel(x, y,20,250,20);
+                    }
+                    else{
+                        g.plotPixel(x, y,0,0,0);
+                    }
                 }
             }
+
+            // Draw Fruit
+            apple.drawFriut(apple.getPoint() , g);
+
+            // Draw Snake
+            hiss.draw(g);
+
+
         }
-    }
-    else if(fun.checkState() == OVER){
-        //if it dies
-        for(int y = 0; y < NUM_ROW + 50; y++){
-            for(int x = 0; x < NUM_COL; x++){
-                g.plotPixel(x, y, 250, 20, 20);
+        else if(fun.checkState() == OVER){
+            for(int y = 0; y < NUM_ROW + SIZE; y++){
+                for(int x = 0; x < NUM_COL; x++){
+                    g.plotPixel(x, y, 250, 20, 20);
+                }
             }
+
         }
-    }
-    else{
-        //start screen
-        for(int y = 0; y < NUM_ROW; y++){
-            for(int x = 0; x < NUM_COL; x++){
-                g.plotPixel(x, y, 255, 255, 255);
-            }
-        }
-    }
 
 
+        g.update();
+        g.Sleep(SPEED);
 
-
-    g.update();
-    g.Sleep(speed);      //takes miliseconds for its argument
 
     }
     return 0;
