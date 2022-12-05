313c61a2e3045e2dae280022cb5c66103c7714fa
KGarza1 <117413915+KGarza1@users.noreply.github.com>
2022-12-05 14:36:23 -0600

    Add files via upload

--- font.h
+++ font.h
@@ -1,10 +1,23 @@
 #ifndef FONT_H_INCLUDED
 #define FONT_H_INCLUDED
 
-void character(int n){
+#include <string>
+#include <fstream>
+#include "SDL_Plotter.h"
 
+using namespace std;
 
+class font{
+private:
+    int number;
+    //int zero[264];
+    //int letter[1274];
+    ifstream iFile;
 
-}
+public:
+    void printFont(string fileName, int yBaseLocation , int xBaseLocation, SDL_Plotter&);
+    void printFontLetter(string fileName, int yBaseLocation , int xBaseLocation, SDL_Plotter&);
+
+};
 
 #endif // FONT_H_INCLUDED
