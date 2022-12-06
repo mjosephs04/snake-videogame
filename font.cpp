b82613a790683f08408b2d08a35c06bb404d1c51
KGarza1 <117413915+KGarza1@users.noreply.github.com>
2022-12-06 15:25:42 -0600

    Add files via upload

--- font.cpp
+++ font.cpp
@@ -1,9 +1,8 @@
-
-
 #include "font.h"
 
 
-void font::printFont(string fileName, int yBaseLocation , int xBaseLocation, SDL_Plotter& g){
+void font::printFont(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int R, int G, int B){
+    ifstream iFile;
     int zero[264];
     iFile.open(fileName);
     for(int x = 0; x < 264; x++){
@@ -14,22 +13,23 @@ void font::printFont(string fileName, int yBaseLocation , int xBaseLocation, SDL
     int row = 0;
     for(int k = 0; k < 264; k++){
         if(zero[k] == 1){
-            for(int y = yBaseLocation; y < yBaseLocation + 5; y++){
-                for(int x = xBaseLocation; x < xBaseLocation + 5; x++){
-                    g.plotPixel(x + space, y + row, 255, 255, 255);
+            for(int y = yBaseLocation; y < yBaseLocation + thickness; y++){
+                for(int x = xBaseLocation; x < xBaseLocation + thickness; x++){
+                    g.plotPixel(x + space, y + row, R, G, G);
                 }
             }
         }
         if(k % 24 == 0){
-            row += 5;
+            row += thickness;
             space = 0;
         }
-        space += 5;
+        space += thickness;
     }
     iFile.close();
 }
 
-void font::printFontLetter(string fileName, int yBaseLocation , int xBaseLocation, SDL_Plotter& g){
+void font::printFontLetter(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int r, int G, int b){
+    ifstream iFile;
     int letter[1274];
     iFile.open(fileName);
     for(int x = 0; x < 1274; x++){
@@ -40,17 +40,17 @@ void font::printFontLetter(string fileName, int yBaseLocation , int xBaseLocatio
     int row = 0;
     for(int k = 0; k < 1274; k++){
         if(letter[k] == 1){
-            for(int y = yBaseLocation; y < yBaseLocation + 1; y++){
-                for(int x = xBaseLocation; x < xBaseLocation + 1; x++){
-                    g.plotPixel(x + space, y + row, 255, 255, 255);
+            for(int y = yBaseLocation; y < yBaseLocation + thickness; y++){
+                for(int x = xBaseLocation; x < xBaseLocation + thickness; x++){
+                    g.plotPixel(x + space, y + row, r, G, b);
                 }
             }
         }
         if(k % 49 == 0){
-            row += 1;
+            row += thickness;
             space = 0;
         }
-        space += 1;
+        space += thickness;
     }
     iFile.close();
 }
