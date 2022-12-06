8fde5bea1590c4a50cd5cca28424b4a8c1b2108a
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-05 22:19:42 -0600

    Add files via upload

--- constants.h
+++ constants.h
@@ -10,7 +10,10 @@
 
 const int NUM_ROW = 700;
 const int NUM_COL = 1000;
-
+const int MAX = 1000;
 const int SIZE = 50;
+const int SPEED = 70;
+
+enum Direction {UP, DOWN, LEFT, RIGHT};
 
 #endif /* constants_h */
