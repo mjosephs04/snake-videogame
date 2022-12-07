8b23f4eccf8690c66a548fdd5782f2fa999fed79
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 14:50:15 -0600

    Update fruit.h

--- fruit.h
+++ fruit.h
@@ -18,20 +18,61 @@ class Fruit{
 private:
     Point_t p;
 public:
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: fruit constructor, doesn't do anything                    *
+    * return: N/A                                                            *
+    * precondition: none                                                     *
+    * postcondition: fruit object exists                                     *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     Fruit(){
-        p.x = rand() %NUM_ROW;
-        p.y = rand() %NUM_COL;
     }
-
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: draw the fruit                                            *
+    * return: void                                                           *
+    * precondition: fruit object exists, and SDL_Plotter exists              *
+    * postcondition: fruit object exists                                     *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void draw(SDL_Plotter&);
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: accessor function for fruit's Point_t                     *
+    * return: Point_t                                                        *
+    * precondition: fruit object exists                                      *
+    * postcondition: nothing is changed, Point is returned                   *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     Point_t getPoint() const;
-
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: mutator function for fruit's Point_t                      *
+    * return: void                                                           *
+    * precondition: fruit object exists, two integers exist                  *
+    * postcondition: fruit's point is changed                                *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void setPoint(int, int);
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: sets fruit's point to a random point on the grid that is  *
+    *              not on top of the snake's current location                *
+    * return: void                                                           *
+    * precondition: fruit object exists, snake object exists                 *
+    * postcondition: the fruit has a new random point                        *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void eatenFruit(snake);
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: get fruit's location from a save file                     *
+    * return: void                                                           *
+    * precondition: fruit object exists, input file stream exists            *
+    * postcondition: fruit's position is changed to saved data from file     *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void initialize(ifstream& fin);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: save fruit's location to a save file                      *
+    * return: void                                                           *
+    * precondition: fruit object exists, output file stream exists           *
+    * postcondition: fruit's position is saved to a data file                *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void saveToFile(ofstream& fout);
 
 
