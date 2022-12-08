1b588465b53ce8a6f2ddd958bcbbce10f68f6424
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 19:14:46 -0600

    Update point.h

--- point.h
+++ point.h
@@ -1,4 +1,10 @@
-
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 #ifndef Point_h
 #define Point_h
 #include "SDL_Plotter.h"
@@ -18,10 +24,6 @@ struct Point_t{
       this->x = x;
       this->y = y;
    }
-
-   //void draw(SDL_Plotter& g, color c = color(0, 0, 0)){
-   //    g.plotPixel(x, y, c);
-   //}
 
    Point_t& operator+ (const Point_t& p){
         Point_t result;
