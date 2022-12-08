41b60730f86697c2dc6e4b88413477dd5188ec74
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 19:13:20 -0600

    Update gameObject.h

--- gameObject.h
+++ gameObject.h
@@ -1,9 +1,10 @@
-//
-//  gameObject.h
-//  groupProject(final)
-//
-//  Created by Caleb Beckering on 12/6/22.
-//
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 
 #ifndef gameObject_h
 #define gameObject_h
@@ -21,17 +22,82 @@ private:
     int score = 0;
     font ffont;
 public:
-
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: Game constructor                                          *
+    * return: N/A                                                            *
+    * precondition: none                                                     *
+    * postcondition: Game object exists                                      *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     Game();
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: checks and returns the GameState                          *
+    * return: GameState                                                      *
+    * precondition: Game object exists                                       *
+    * postcondition: GameState is returned                                   *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     GameState checkState();
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: change the GameState                                      *
+    * return: void                                                           *
+    * precondition: GameState exists, game object exists                     *
+    * postcondition: GameState is changed                                    *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void changeState(GameState);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: add one point to the score                                *
+    * return: void                                                           *
+    * precondition: game object exists                                       *
+    * postcondition: score is incremented                                    *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void addPoint();
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: returns the current score                                 *
+    * return: integer                                                        *
+    * precondition: game object exists                                       *
+    * postcondition: nothing is changed, score is returned                   *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     int getScore();
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: displays the score to the screen                          *
+    * return: void                                                           *
+    * precondition: game object exists, SDL_Plotter exists                   * 
+    * postcondition: score is displayed, nothing is changed                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void displayYourScore(SDL_Plotter&);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: resets the score to 0                                     *
+    * return: void                                                           *
+    * precondition: game object exists                                       *
+    * postcondition: score is set to 0                                       *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void resetScore();
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: initializes the score to whatever is stored in save file  *
+    * return: void                                                           *
+    * precondition: input file stream exists, game object exists             *
+    * postcondition: score is initialized to whatever is stored in file      *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void initialize(ifstream&);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: saves current score to save file                          *
+    * return: void                                                           *
+    * precondition: output file stream exists, game object exists            *
+    * postcondition: score is saved to file                                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void saveToFile(ofstream&);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: displays score to screen - formatted for in-game display  *
+    * return: void                                                           *
+    * precondition: SDL_Plotter exists, game object exists                   *
+    * postcondition: score is printed to screen, nothing is changed          *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreInGame(SDL_Plotter&);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: prints score - formatted for game over screen             *
+    * return: void                                                           *
+    * precondition: SDL_Plotter exists, game object exists                   *
+    * postcondition: score is printed to screen, nothing is changed          *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScorePostGame(SDL_Plotter&);
 
 };
