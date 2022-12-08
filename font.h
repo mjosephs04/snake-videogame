7b6941e4398e89d785a3e01dddacf40c3dafbb92
mjosephs04 <95541571+mjosephs04@users.noreply.github.com>
2022-12-07 20:04:10 -0600

    Update font.h

--- font.h
+++ font.h
@@ -23,39 +23,42 @@ private:
     
 public:
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * description:                    
-    * return:                                                            
-    * precondition:                                                      
-    * postcondition:                                  
+    * description: prints the 
+    * return: void
+    * precondition: 6 integers, one string, and one plotter
+    * postcondition: prints a number on the plotter
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printFontNumber(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int R, int G, int B);
+    
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * description:                    
-    * return:                                                            
-    * precondition:                                                      
-    * postcondition:                                  
+    * description: prints a letter on the screen
+    * return: void
+    * precondition: one string, 6 integers and one plotter
+    * postcondition: letter is printed on the plotter
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printFontLetter(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int, int, int);
 
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * description:                    
-    * return:                                                            
-    * precondition:                                                      
-    * postcondition:                                  
+    * description: prints the score when it is a single digit
+    * return: void
+    * precondition: four integers, plotter and a font
+    * postcondition: printed score on the screen when it is a single digit
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreSingle(int, font&, SDL_Plotter&, int, int, int = 3);
+    
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * description:                    
-    * return:                                                            
-    * precondition:                                                      
-    * postcondition:                                  
+    * description: prints the score when it is two digits
+    * return: void
+    * precondition: six integers, a font class and SDL plotter class
+    * postcondition: printed score on the screen when it is a double digit number
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreDouble(int, font&, SDL_Plotter&, int, int, int, int, int = 3);
+    
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * description:                    
-    * return:                                                            
-    * precondition:                                                      
-    * postcondition:                                  
+    * description: prints the score when it is three digits
+    * return: void
+    * precondition: eight integers, a font class and SDL Plotter class
+    * postcondition: printed score on the screen when it is a tripe digit number
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreTriple(int, font&, SDL_Plotter&, int, int, int, int, int, int, int = 3);
 
