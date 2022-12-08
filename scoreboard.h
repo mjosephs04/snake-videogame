9db025eb2fcee5c03a4f7713270c966e30793f11
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 19:21:06 -0600

    Update scoreboard.h

--- scoreboard.h
+++ scoreboard.h
@@ -1,9 +1,10 @@
-//
-//  scoreboard.h
-//  groupProject(final)
-//
-//  Created by Caleb Beckering on 12/5/22.
-//
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 
 #ifndef scoreboard_h
 #define scoreboard_h
