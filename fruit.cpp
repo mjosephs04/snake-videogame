45d3b6fb8ec90f91f3a075cb33186c54213acdb0
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 18:54:54 -0600

    Update fruit.cpp

--- fruit.cpp
+++ fruit.cpp
@@ -1,9 +1,10 @@
-//
-//  fruit.cpp
-//  SDL Practice
-//
-//  Created by Caleb Beckering on 11/16/22.
-//
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 
 #include <stdio.h>
 #include "fruit.h"
