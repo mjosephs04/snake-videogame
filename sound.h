5937a1ed6e37b2a66f84f1a40def0cfd59f846e7
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 18:50:29 -0600

    Update sound.h

--- sound.h
+++ sound.h
@@ -13,13 +13,34 @@ private:
     string eatingSoundFileName = "eating-apple.wav";
 
 public:
-
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: Sound constructor                                         *
+    * return: N/A                                                            *
+    * precondition: SDL Plotter exists, there is a dying sound and eating    *
+    *               apple sound file                                         *
+    * postcondition: sound object exists and the files are initialized       *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     Sound(SDL_Plotter& g){
         g.initSound(dyingSoundFileName);
         g.initSound(eatingSoundFileName);
     }
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: plays dying sound                                         *
+    * return: void                                                           *
+    * precondition: SDL_Plotter exists, Sound object exists and is           *
+    *               initialized                                              *
+    * postcondition: nothing is changed, sound is played                     *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void dying(SDL_Plotter&);
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: plays eating sound                                        *
+    * return: void                                                           *
+    * precondition: SDL_Plotter exists, Sound object exists and is           *
+    *               initialized                                              *
+    * postcondition: nothing is changed, sound is played                     *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void eating(SDL_Plotter&);
 
 };
