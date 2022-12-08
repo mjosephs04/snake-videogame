a22ecdc2430c2689398cdbbb5cfe7f017fa5da02
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 19:21:31 -0600

    Update sound.cpp

--- sound.cpp
+++ sound.cpp
@@ -1,3 +1,10 @@
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 
 #include "sound.h"
 
