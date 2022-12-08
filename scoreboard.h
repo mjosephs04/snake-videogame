9e1297598a603adfd3bc68e863c64ba0095359da
mjosephs04 <95541571+mjosephs04@users.noreply.github.com>
2022-12-07 20:43:22 -0600

    Update scoreboard.h

--- scoreboard.h
+++ scoreboard.h
@@ -16,10 +16,44 @@ private:
     int highScore;
     font ffont;
 public:
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: saves position of snake to a file                         *
+    * return: void                                                           *
+    * precondition: output stream                                            *
+    * postcondition: saves position to a file                                *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void saveToFile(ofstream&);
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: initializes the scoreboard                                *
+    * return: void                                                           *
+    * precondition: input stream                                             *
+    * postcondition: scoreboard is initialized                               *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void initialize(ifstream&);
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: getting the high score                                    *
+    * return: integer                                                        *
+    * precondition: there is a score                                         *
+    * postcondition: returns the highscore of the user                       *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     int getHighScore();
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: highscore is set for program                              *
+    * return: void                                                           *
+    * precondition: an integer                                               *
+    * postcondition: the highscore is set                                    *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void setHighScore(int);
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description: prints the highscore on the plotter                       *
+    * return: void                                                           *
+    * precondition: sdl plotter and a font class                             *
+    * postcondition: the highscore is plotted on the screen                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printHighScore(SDL_Plotter&, font&);
 };
 
