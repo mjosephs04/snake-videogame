eba075dd080c6606b5e216cff295e4044dd1133b
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 18:54:27 -0600

    Update font.h

--- font.h
+++ font.h
@@ -1,3 +1,11 @@
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
+
 #ifndef FONT_H_INCLUDED
 #define FONT_H_INCLUDED
 
@@ -12,14 +20,43 @@ using namespace std;
 class font{
 private:
     int number;
-
-
+    
 public:
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description:                    
+    * return:                                                            
+    * precondition:                                                      
+    * postcondition:                                  
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printFontNumber(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int R, int G, int B);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description:                    
+    * return:                                                            
+    * precondition:                                                      
+    * postcondition:                                  
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printFontLetter(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int, int, int);
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description:                    
+    * return:                                                            
+    * precondition:                                                      
+    * postcondition:                                  
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreSingle(int, font&, SDL_Plotter&, int, int, int = 3);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description:                    
+    * return:                                                            
+    * precondition:                                                      
+    * postcondition:                                  
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreDouble(int, font&, SDL_Plotter&, int, int, int, int, int = 3);
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * description:                    
+    * return:                                                            
+    * precondition:                                                      
+    * postcondition:                                  
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void printScoreTriple(int, font&, SDL_Plotter&, int, int, int, int, int, int, int = 3);
 
 };
