59c3d5b4e7f2e782764e2566fdfee972dfaf6c01
chuanyi2004 <117413693+chuanyi2004@users.noreply.github.com>
2022-12-07 18:52:22 -0600

    Update font.cpp

--- font.cpp
+++ font.cpp
@@ -1,5 +1,11 @@
+/* AUTHOR: Caleb Beckering, Ruth Edwards, Karina Garza, Mark Josephs
+ * ASSIGNMENT TITLE: Group Project: Snake
+ * ASSIGNMENT DESCRIPTION: Create a snake-themed game
+ * DUE DATE: 12-07-22
+ * DATE CREATED: 11-03-22
+ * DATE LAST MODIFIED: 12-07-22
+ */
 #include "font.h"
-
 
 void font::printFontNumber(string fileName, int xBaseLocation , int yBaseLocation, SDL_Plotter& g, int thickness, int R, int G, int B){
     ifstream iFile;
